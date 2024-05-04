0ce7ed25afa92f0b0a88a0ab3e550909102461ca
Georgii Antonevich <easylanding.online@gmail.com>
2024-05-04 11:03:54 +0300

    work with gorutine

--- main.go
+++ main.go
@@ -31,18 +31,47 @@ func main() {
 	helloWorld := HelloWorld()
 	fmt.Println(helloWorld)
 
-	timeoutFunc := timeout(3 * time.Second)
-	since := time.NewTimer(3050 * time.Millisecond)
-	for {
-		select {
-		case <-since.C:
-			fmt.Println("Функция не выполнена вовремя")
-			return
-		default:
-			if timeoutFunc() {
-				fmt.Println("Функция выполнена вовремя")
+	// timeoutFunc := timeout(3 * time.Second)
+	// since := time.NewTimer(3050 * time.Millisecond)
+	// for {
+	// 	select {
+	// 	case <-since.C:
+	// 		fmt.Println("Функция не выполнена вовремя")
+	// 		return
+	// 	default:
+	// 		if timeoutFunc() {
+	// 			fmt.Println("Функция выполнена вовремя")
+	// 			return
+	// 		}
+	// 	}
+	// }
+
+	// Создаем новый тикер с интервалом 1 секунда
+	ticker := time.NewTicker(1 * time.Second)
+
+	data := NotifyEvery(ticker, 5*time.Second, "Таймер сработал")
+
+	for v := range data {
+		fmt.Println(v)
+	}
+
+	fmt.Println("Программа завершена")
+}
+
+func NotifyEvery(ticker *time.Ticker, d time.Duration, message string) <-chan string {
+	data := make(chan string)
+
+	go func() {
+		defer close(data)
+		for {
+			select {
+			case <-ticker.C:
+				data <- message
+			case <-time.After(d):
 				return
 			}
 		}
-	}
+	}()
+
+	return data
 }
